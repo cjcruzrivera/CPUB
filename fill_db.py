203b0a071bf834925b008cc0e0d6b23912852e3a
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2019-11-08 10:35:13 -0500

    Llenado de la base de datos

--- fill_db.py
+++ fill_db.py
@@ -1,14 +1,49 @@
 import sqlite3
 import sys
 from faker import Faker
+from random import randint
 conn = sqlite3.connect('database.db')
+c = conn.cursor()
 print ("Opened database successfully")
+fake = Faker()
 
+marcas = ["GW",
+                "SHIMANO",
+                "ROCKSHOCK",
+                "BOSTON",
+                "OPTIMUS",
+                "TREK",
+                "GRAVITY"]
+colores = ["AZUL", "ROJO", "VERDE", "NEGRO", "BLANCO", "AMARILLO", "MORADO", "GRIS"]
+modelos = ["MTB", "RUTA", "CIUDAD", "PLAYERA", "URBANA"]
+propietarios = []
+for i in range(1,101):
+    documento = fake.msisdn()
+    nombre = fake.name()
+    telefono = fake.msisdn()
+    email = fake.email()
+    propietarios.insert(0,documento)
+    insert_prop = "INSERT INTO propietario VALUES ('{}', '{}', '{}', '{}')".format(documento, nombre, telefono, email)
+    print("insert_prop")
+    c.execute(insert_prop)
 
-conn.execute(sql_create_propietario)
-conn.execute(sql_create_bicicleta)
-conn.execute(sql_create_antecedentes)
-print("CREATED TABLES")
-# conn.execute('CREATE TABLE bicicleta (serial TEXT, addr TEXT, city TEXT, pin TEXT)')
-# print "Table created successfully";
+for i in range(1,201):
+    serial = fake.isbn10(separator='-')
+    marca = marcas[randint(0,(len(marcas)-1))]
+    modelo = modelos[randint(0,(len(modelos)-1))]
+    color = colores[randint(0,(len(colores)-1))]
+    propietario = propietarios[randint(0,(len(propietarios)-1))]
+    insert_bici = "INSERT INTO bicicleta VALUES ('{}', '{}', '{}', '{}', '{}')".format(serial, marca, modelo, color, propietario)
+    c.execute(insert_bici)
+    print("insert_bici")
+
+    if randint(0,1):
+        if randint(0,1):
+            if randint(0,1):
+                antecedente = "Se denuncia su robo el dia " + fake.date()
+                insert_ant = "INSERT INTO antecedente(serial, antecedente) VALUES('{}', '{}')".format(serial, antecedente)
+                c.execute(insert_ant)
+                print("insert_ant")
+c.close()
+conn.commit()
 conn.close()
