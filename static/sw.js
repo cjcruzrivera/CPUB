60ee8ee94531c268641375eccbc1615407e44859
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2019-11-12 08:56:54 -0500

    self

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
+self.addEventListener('beforeinstallprompt', (e) => {
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI notify the user they can add to home screen
