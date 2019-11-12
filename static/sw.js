5c23d87bb9ee90184db2f47a06b7c77b46f8a65f
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2019-11-12 09:27:04 -0500

    modify sw

--- static/sw.js
+++ static/sw.js
@@ -1,45 +1,48 @@
-(function() {
-    if('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-                 .then(function(registration) {
-                 console.log('Service Worker Registered');
-                 return registration;
-        })
-        .catch(function(err) {
-          console.error('Unable to register service worker.', err);
-        });
-        navigator.serviceWorker.ready.then(function(registration) {
-          console.log('Service Worker Ready');
-        });
-      });
+console.log('Hello from sw.js');
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+
+  workbox.precaching.precacheAndRoute([
+    {
+      "url": "/",
+      "revision": "1"
     }
-  })();
-  
-  let deferredPrompt;
-  const btnAdd = document.querySelector('#btnAdd');
-  
-  window.addEventListener('beforeinstallprompt', (e) => {
-    console.log('beforeinstallprompt event fired');
-    e.preventDefault();
-    deferredPrompt = e;
-    btnAdd.style.visibility = 'visible';
-  });
-  
-  btnAdd.addEventListener('click', (e) => {
-    btnAdd.style.visibility = 'hidden';
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
-  
-  window.addEventListener('appinstalled', (evt) => {
-    app.logEvent('app', 'installed');
-  });
+  ]);
+
+  workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'static-resources',
+    }),
+  );
+
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    }),
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'googleapis',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 30,
+        }),
+      ],
+    }),
+  );
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
