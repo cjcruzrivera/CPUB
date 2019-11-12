1520151977d1ca3e5efc33166532e7b97ff8060e
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2019-11-12 08:54:58 -0500

    .

--- static/sw.js
+++ static/sw.js
@@ -4,5 +4,5 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI notify the user they can add to home screen
-    deferredPrompt.prom
+    deferredPrompt.prompt()
 });
